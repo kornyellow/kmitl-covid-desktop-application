3bd7e64ace6b9c12ad64fe2b08139d049cbf7935
kornyellow <kornkubzaza@gmail.com>
2022-04-15 16:55:55 +0700

    Remove Hello

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -13,7 +13,6 @@ public class Application extends javafx.application.Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	// Hello
 
 	public static void main(String[] args) {
 		Application.launch();
