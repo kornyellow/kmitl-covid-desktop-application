388193593dc3daf63943e9bfcc8269706f42fea0
kornyellow <kornkubzaza@gmail.com>
2022-04-17 05:46:31 +0700

    Delete unnecessary files

--- src/main/java/application/Debug.java
+++ src/main/java/application/Debug.java
@@ -3,9 +3,12 @@ package application;
 import lib.classes.staff.Staff;
 import lib.methods.staff.CVStaff;
 
-import java.util.ArrayList;
-
 public class Debug {
 	public static void main(String[] args) {
+		Staff staff = new Staff();
+		staff.setFirstName("Korn");
+		staff.setLastName("Rojrattanapanya");
+
+		CVStaff.insertStaff(staff);
 	}
 }
