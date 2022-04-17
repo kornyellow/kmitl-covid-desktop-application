ceff6754054f6cd59ae6309ac7f0edfa24a98f84
Apinyawat Khwanpruk <sanfong2222@gmail.com>
2022-04-17 08:59:10 +0700

    Fix Font Including

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -3,6 +3,7 @@ package application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lib.korn.kornutil.KornFix;
+import lib.korn.kornutil.KornFont;
 import other.Config;
 import template.Home;
 
@@ -10,6 +11,10 @@ public class Application extends javafx.application.Application {
 	@Override
 	public void start(Stage primaryStage) {
 		KornFix.fixAll();
+		KornFont.fontPathLight = this.getClass().getClassLoader().getResource("GraphikThaiLoop-Light.otf").toString();
+		KornFont.fontPathNormal = this.getClass().getClassLoader().getResource("GraphikThaiLoop-Regular.otf").toString();
+		KornFont.fontPathBold = this.getClass().getClassLoader().getResource("GraphikThaiLoop-Semibold.otf").toString();
+		KornFont.loadAllFont();
 		Scene scene = new Scene(Home.getLayout(), Config.applicationWidth, Config.applicationHeight);
 		primaryStage.setTitle(Config.applicationTitle);
 		primaryStage.setMaximized(true);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module application {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
-	requires mysql.connector.java;
 
 	opens application to javafx.fxml;
 	exports application;
