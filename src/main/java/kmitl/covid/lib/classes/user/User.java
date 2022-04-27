92eb45ae36c3e9f9a42843ab94634a1b989247ae
Thongthana Laemkaothong <porporthongthana@gmail.com>
2022-04-27 21:13:27 +0700

    Update class user ver. 1.2

--- src/main/java/kmitl/covid/lib/classes/user/User.java
+++ src/main/java/kmitl/covid/lib/classes/user/User.java
@@ -13,7 +13,10 @@ public class User {
 	}
 
 	private final int id;
-	private EnumNameTitle nameTitle;
+    private String username;
+    private String password;
+    private KornDateTime lastLogin;
+    private EnumNameTitle nameTitle;
 	private String firstName;
 	private String lastName;
 	private EnumGender gender;
@@ -22,55 +25,95 @@ public class User {
 	private String address;
 	private String telephoneNumber;
 
-	public int getId() {
-		return id;
-	}
-	public EnumNameTitle getNameTitle() {
-		return nameTitle;
-	}
-	public void setNameTitle(EnumNameTitle nameTitle) {
-		this.nameTitle = nameTitle;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public EnumGender getGender() {
-		return gender;
-	}
-	public void setGender(EnumGender gender) {
-		this.gender = gender;
-	}
-	public String getNationalID() {
-		return nationalID;
-	}
-	public void setNationalID(String nationalID) {
-		this.nationalID = nationalID;
-	}
-	public KornDateTime getBirthDate() {
-		return birthDate;
-	}
-	public void setBirthDate(KornDateTime birthDate) {
-		this.birthDate = birthDate;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getTelephoneNumber() {
-		return telephoneNumber;
-	}
-	public void setTelephoneNumber(String telephoneNumber) {
-		this.telephoneNumber = telephoneNumber;
-	}
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public KornDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(KornDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public EnumNameTitle getNameTitle() {
+        return nameTitle;
+    }
+
+    public void setNameTitle(EnumNameTitle nameTitle) {
+        this.nameTitle = nameTitle;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public EnumGender getGender() {
+        return gender;
+    }
+
+    public void setGender(EnumGender gender) {
+        this.gender = gender;
+    }
+
+    public String getNationalID() {
+        return nationalID;
+    }
+
+    public void setNationalID(String nationalID) {
+        this.nationalID = nationalID;
+    }
+
+    public KornDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(KornDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
 }
