6d1a51dd02ea429ebef534a970a81900d196544e
kornyellow <kornkubzaza@gmail.com>
2022-05-01 21:38:12 +0700

    Added DateTime

--- src/main/java/kmitl/covid/lib/classes/notification/Notification.java
+++ src/main/java/kmitl/covid/lib/classes/notification/Notification.java
@@ -1,6 +1,7 @@
 package kmitl.covid.lib.classes.notification;
 
 import kmitl.covid.lib.classes.user.User;
+import kmitl.covid.lib.korn.kornutil.KornDateTime;
 
 public class Notification {
 	public Notification() {
@@ -12,28 +13,35 @@ public class Notification {
 
 	private final int id;
 	private User user;
-	private String text;
+	private String data;
+	private KornDateTime date;
 	private boolean isRead;
 
 	public int getId() {
-		return id;
+		return this.id;
 	}
 	public User getUser() {
-		return user;
+		return this.user;
 	}
 	public void setUser(User user) {
 		this.user = user;
 	}
-	public String getText() {
-		return text;
+	public String getData() {
+		return this.data;
 	}
-	public void setText(String text) {
-		this.text = text;
+	public void setText(String data) {
+		this.data = data;
+	}
+	public KornDateTime getDate() {
+		return this.date;
+	}
+	public void setDate(KornDateTime date) {
+		this.date = date;
 	}
 	public boolean isRead() {
-		return isRead;
+		return this.isRead;
 	}
-	public void setRead(boolean read) {
-		isRead = read;
+	public void setIsRead(boolean isRead) {
+		this.isRead = isRead;
 	}
 }
