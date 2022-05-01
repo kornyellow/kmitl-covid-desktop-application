ee0e65fa06cce8f534b58bab888d1e8e51b1b874
kornyellow <kornkubzaza@gmail.com>
2022-05-01 21:09:23 +0700

    Update dashboard

--- src/main/java/kmitl/covid/content/dashboard/DashboardPage.java
+++ src/main/java/kmitl/covid/content/dashboard/DashboardPage.java
@@ -1,11 +1,17 @@
 package kmitl.covid.content.dashboard;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import kmitl.covid.lib.korn.kornutil.KornFont;
 
 public class DashboardPage {
 	public static GridPane getContent() {
 		if (DashboardPage.node != null) return DashboardPage.node;
 		DashboardPage.node = new GridPane();
+
+		Label label = new Label("หน้าแรก");
+		label.setFont(KornFont.bigHeaderBold);
+		DashboardPage.node.add(label, 0, 0);
 
 		return DashboardPage.node;
 	}
