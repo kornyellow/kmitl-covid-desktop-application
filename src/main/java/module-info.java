b0e769916f93df28c7bab00c7251d40360534ad5
kornyellow <kornkubzaza@gmail.com>
2022-05-06 19:23:16 +0700

    Added Bootstrap to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module kmitl.covid {
 	requires javafx.fxml;
 	requires java.sql;
 	requires bcrypt;
+	requires org.kordamp.bootstrapfx.core;
 
 	opens kmitl.covid.launcher to javafx.fxml;
 	exports kmitl.covid.launcher;
