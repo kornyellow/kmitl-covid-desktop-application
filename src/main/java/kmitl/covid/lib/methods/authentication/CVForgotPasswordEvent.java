f9e2166f7eba5c369139aca6f52018ee5ee3e42f
Game <86363360+Vive14@users.noreply.github.com>
2022-05-06 16:39:10 +0700

    add CVForgotPasswordEvent

---
+++ src/main/java/kmitl/covid/lib/methods/authentication/CVForgotPasswordEvent.java
@@ -0,0 +1,56 @@
+package kmitl.covid.lib.methods.authentication;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+import kmitl.covid.lib.enums.EnumAlertType;
+import kmitl.covid.lib.enums.EnumPage;
+import kmitl.covid.lib.korn.kornutil.KornAlert;
+import kmitl.covid.template.Home;
+
+public class CVForgotPasswordEvent {
+	public static EventHandler<ActionEvent> ForgotpasswordEvent(
+		TextField email,
+		TextField nationalID,
+		TextField newPassword
+	) {
+		return actionEvent -> {
+			String emailText = email.getText();
+			String nationalIDText = nationalID.getText();
+			String newPasswordText = newPassword.getText();
+
+			if (emailText.isEmpty() && nationalIDText.isEmpty() && newPasswordText.isEmpty()) {
+				KornAlert.alert(
+					EnumAlertType.ERROR,
+					"ไม่สามารถเข้าสู่ระบบได้",
+					"กรุณากรอกข้อมูลให้ครบถ้วน"
+				);
+				email.requestFocus();
+				return;
+			}
+
+			if (emailText.isEmpty()) {
+				email.requestFocus();
+				return;
+			}
+
+			if (nationalIDText.isEmpty()) {
+				nationalID.requestFocus();
+				return;
+			}
+
+			if (newPasswordText.isEmpty()) {
+				newPassword.requestFocus();
+				return;
+			}
+
+			KornAlert.alert(
+				EnumAlertType.SUCCESS,
+				"เปลี่ยนรหัสผ่านเรียบร้อย",
+				""
+			);
+
+			Home.redirect(EnumPage.LOGIN());
+		};
+	}
+}
