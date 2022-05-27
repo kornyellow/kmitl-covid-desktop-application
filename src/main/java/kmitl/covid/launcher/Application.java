110dcfb0855819382ebaa3a1683649f36a6f6184
kornyellow <kornkubzaza@gmail.com>
2022-05-27 13:58:57 +0700

    Added Icon

--- src/main/java/kmitl/covid/launcher/Application.java
+++ src/main/java/kmitl/covid/launcher/Application.java
@@ -1,14 +1,17 @@
 package kmitl.covid.launcher;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import kmitl.covid.Launcher;
 import kmitl.covid.lib.enums.EnumPage;
-import kmitl.covid.lib.korn.kornutil.KornFix;
 import kmitl.covid.lib.korn.kornresource.KornResource;
+import kmitl.covid.lib.korn.kornutil.KornFix;
 import kmitl.covid.other.Config;
 import kmitl.covid.template.Home;
 
 import java.util.Locale;
+import java.util.Objects;
 
 public class Application extends javafx.application.Application {
 	@Override
@@ -18,6 +21,10 @@ public class Application extends javafx.application.Application {
 		KornResource.loadResource();
 
 		Application.primaryStage = primaryStage;
+
+		String iconPath = Objects.requireNonNull(Launcher.class.getResource("image/icon.png")).toString();
+		Application.primaryStage.getIcons().add(new Image(iconPath));
+
 		Application.primaryStage.setScene(Home.getScene(EnumPage.LOGIN()));
 		Application.primaryStage.setTitle(Config.applicationTitle);
 		Application.primaryStage.setMaximized(true);
